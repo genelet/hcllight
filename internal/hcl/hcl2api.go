8b0a77a3d507340e1e32f109ebb6b9d2b4750b05
Peter Bi <genelet@gmail.com>
2024-04-27 14:32:33 -0400

    backup

---
+++ internal/hcl/hcl2api.go
@@ -0,0 +1,15 @@
+package hcl;
+
+import (
+	openapiv3 "github.com/google/gnostic-models/openapiv3"
+)
+
+func TagToHcl(tag *openapiv3.Tag) *Tag {
+	t := &Tag{
+		
+	}
+}
+
+function DocumentToHcl(doc *openapiv3.Document) *Document {
+
+}
